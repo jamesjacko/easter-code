2d6fc9aada25d8a61c98caa64efc397120405f6a
James <james.ogge@gmail.com>
2020-04-12 12:58:03 +0100

    Updates

---
+++ precache-manifest.1b4e2b0d81615faeada91879c67a62a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccb909b0ad3271d99936dcade3d51c3",
+    "url": "/easter-code/index.html"
+  },
+  {
+    "revision": "8b31b383653f0c80ecf2",
+    "url": "/easter-code/static/css/main.faa8a1ec.chunk.css"
+  },
+  {
+    "revision": "771391d39337903afc31",
+    "url": "/easter-code/static/js/2.c0e497ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/easter-code/static/js/2.c0e497ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b31b383653f0c80ecf2",
+    "url": "/easter-code/static/js/main.4c3dc544.chunk.js"
+  },
+  {
+    "revision": "bae42ed38c02d47702f4",
+    "url": "/easter-code/static/js/runtime-main.a3ee06e3.js"
+  },
+  {
+    "revision": "796b643a146db4b34af5f263f2b19e4a",
+    "url": "/easter-code/static/media/animalCrossing.796b643a.ttf"
+  },
+  {
+    "revision": "51a33d0eaaddac37d557ee80df331d20",
+    "url": "/easter-code/static/media/bg.51a33d0e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/easter-code/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4e28f3e76088438bfa8004792abe44e5",
+    "url": "/easter-code/static/media/zelda.4e28f3e7.mp3"
+  }
+]);
